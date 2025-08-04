ba21c9c2c3a7129ac44ce3c751f9f2fda595aa20
hamesomer11 <hamzaomer45@gmail.com>
2025-08-04 21:52:14 +0100

    app-build - Commit #2

--- server_test.go
+++ server_test.go
@@ -1,36 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type StubPlayerStore struct {
+	score map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.score[name]
+	return score
+}
+
 func TestGETPlayers(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+
+	server := &PlayerServer{&store}
+
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		request := NewGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		expected := "20"
-
-		if got != expected {
-			t.Errorf("got %q, expected %q", got, expected)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := NewGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		expected := "10"
-
-		if got != expected {
-			t.Errorf("got %q, expected %q", got, expected)
-		}
+		assertResponseBody(t, response.Body.String(), "10")
 	})
+}
+
+func NewGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, expected string) {
+	t.Helper()
+	if got != expected {
+		t.Errorf("response body is wrong, got %q, expected %q", got, expected)
+	}
 }

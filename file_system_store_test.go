6d5e90321576a87046ee471bd8d5134e008a9aa7
hamesomer11 <hamzaomer45@gmail.com>
2025-08-07 23:54:51 +0100

    app-build - database - Commit #3

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 
 	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -27,17 +30,55 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
 		expected := 33
-
-		if got != expected {
-			t.Errorf("got %d, expected %d", got, expected)
-		}
+		assertScoreEquals(t, got, expected)
 	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+		{"Name": "Cleo", "Wins": 10},
+		{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Chris")
+
+		got := store.GetPlayerScore("Chris")
+		expected := 34
+		assertScoreEquals(t, got, expected)
+	})
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
+}
+
+func assertScoreEquals(t testing.TB, got, expected int) {
+	t.Helper()
+	if got != expected {
+		t.Errorf("got %d, expected %d", got, expected)
+	}
 }
